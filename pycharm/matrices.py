74d0747438a69369312e02f6bb076b1c2947fe9b
annaty <tylkowskanna@gmail.com>
2020-08-07 18:22:12 +0200

    Numeric Matrix Processor - part 1 finished

--- pycharm/matrices.py
+++ pycharm/matrices.py
@@ -8,17 +8,34 @@ def getDimensions(dim):
       
 matrixA = []
 matrixB = []
- 
 def createMatrix(dim, emptymatrix):
+    #create the empty list skeleton
     for row in range(0, dim[0]):
         emptymatrix.append([])
-        datainrow = input()
+        datainput = input()
 
-        for char in datainrow:
+        #transform the datainput into integers and append them to the matrix
+        for char in datainput:
             if char != " ":
                 emptymatrix[row].append(int(char))
 
-                
+def addMatrices(mx1, mx2):
+    result = []
+    if dimA != dimB:
+        print("ERROR")
+    else:
+        for row in range(0, dimA[0]):
+            #create the empty list skeleton
+            result.append([])
+            #adding the values from mx1 and mx2, and appending it to the list "result"
+            for element in range(0, dimA[1]):
+                addition = mx1[row][element] + mx2[row][element]
+                result[row].append([addition])
+    
+    return result
+
 getDimensions(dimA)   
 createMatrix(dimA, matrixA)
-print(matrixA)
+getDimensions(dimB)   
+createMatrix(dimB, matrixB)
+print(addMatrices(matrixA, matrixB))
