1d4fea655ecfd5c6ee2271d78fd515ec6d341ebb
annaty <tylkowskanna@gmail.com>
2020-08-28 15:40:13 +0200

    big refactor - introducing list comprehension

--- pycharm/matrices.py
+++ pycharm/matrices.py
@@ -1,68 +1,29 @@
-dimA = []
-dimB = []
-mxA = []
-mxB = []
-
-#def getDimensions(dimentionlist):
-#    lenhi = input()
-#    for char in lenhi:
-#        if char != " ":
-#            dimentionlist.append(int(char))
-      
-def createMatrix(emptydims, emptymatrix):
-    emptydims = [int(_) for _ in input().split()]
+def getDimensions():
+    dims = [int(_) for _ in input().split()]
+    return dims
+    
+def createMatrix(dims):
     #creating the matrix from input, then transforming their content into integers
-    emptymatrix = [input().split() for row in range(emptydims[0])]
-    emptymatrix = [[int(element) for element in row]for row in emptymatrix]
-    return emptydims
-
+    matrix = [input().split() for row in range(dims[0])]
+    matrix = [[int(element) for element in row]for row in matrix]
+    return matrix
 
 def addMatrices(mx1, mx2):
     result = []
     if dimA != dimB:
         print("ERROR")
     else:
-        for row in range(0, dimA[0]):
-            #on each iteration add a row
-            result.append([])
-            #adding the values from mx1 and mx2, and appending it to the list "result"
-            for element in range(0, dimA[1]):
-                addition = mx1[row][element] + mx2[row][element]
-                result[row].append([addition])
-    
-    #making the result pretty
-    counter = 0
-    result_clean = []
-    for row in result:
-        if counter > 0:
-            result_clean.append("\n")
-        for element in row:
-            result_clean.append(str(element).strip("[]"))
-        counter += 1
-    return " ".join(result_clean)
+        result = [[mx1[row][element] + mx2[row][element] for element in range(0, dimA[1])] for row in range(0, dimA[0])]
+        return result
 
 def multiByConst(matrix):
-    const = int(input())
-    result = []
-    counter = 0
-    for row in matrix:
-        result.append([])
-        for element in row:
-            result[counter].append(element * const)
-        counter += 1
-    #making the result pretty
-    counter1 = 0
-    result_clean = []
-    for row in result:
-        if counter1 > 0:
-            result_clean.append("\n")
-        for element in row:
-            result_clean.append(str(element).strip("[]") + " ")
-        counter1 += 1
-    return "".join(result_clean)
+    const = float(input())
+    result = [[element * const for element in row] for row in matrix]
+    return result
 
+dimA = getDimensions()
+mxA = createMatrix(dimA)
+dimB = getDimensions()
+mxB = createMatrix(dimB)
 
-createMatrix(dimA, mxA)
-print(dimA)
-print(mxA)
 
